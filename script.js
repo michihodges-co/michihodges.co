82a5d49c58632d7cd8d28e7d363dfdc081750be5
michihodges <michihodges@gmail.com>
2019-05-07 15:57:23 +0200

    blink

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    var f = document.getElementById('blink');
+    setInterval(function() {
+        f.style.display = (f.style.display == 'none' ? '' : 'none');
+    }, 500);
+
+}, false);
